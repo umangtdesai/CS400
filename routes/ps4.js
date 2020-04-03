fe26677698fe9869735aa62c1e2e63e7d2509852
Umang Desai <umang.t.desai@gmail.com>
2020-04-03 03:35:54 -0400

    Working condition, API removed and emailed key to myself

--- routes/ps4.js
+++ routes/ps4.js
@@ -1,37 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const pugDisplay = require('pug')
-// /const display = pugDisplay.compileFile('../views/ps4Pug.pug');
-const request = require('../node_modules/requests');
+//const pugDisplay = require('pug')
+const request = require('../node_modules/request');
 
 router.route('/')
 
     .get(function (req, res, next) {
             // API key for Musixmatch -- to get song lyrics
-            const key = '738ccb4368c53ac30338df0bf1e32807';
-            // Ed Sheeran song
+            const key = ' MY API KEY GOES HERE';
             const artist = 'Ed Sheeran'
-            const songName = 'Cold Coffee';
-            const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=callback&q_track=cold%20coffee&q_artist=ed%20sheeran&apikey=738ccb4368c53ac30338df0bf1e32807`;
+            const songName = 'Cold Coffee'
 
+            // make sure format = json and not jsonp
+            const url = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=${songName}&q_artist=${artist}&apikey=${key}`;
 
-           // res.render('ps4Pug', {songLine: 'hello'});
-
-            // this doesn't work
-            request(url, function(err, response, bodytext)  {
+            request(url, function(err, response, body)  {
                 if (err) {
                     console.log('An error occured', err);
                     res.render('ps4Pug', { songLine: 'ERROR IN CALL' });
                 }
                 else {
-                    console.log('in else case');
-                    let result = JSON.parse(bodytext);
-                    // Is this correct?
-                    const lyrics = result.body.lyrics.lyrics_body;
+                    //console.log('in else case');
+                    const result = JSON.parse(body);
+                    const lyrics = result.message.body.lyrics.lyrics_body;
+                    //console.log(lyrics);
                     // extract the lyrics and pug render
                     res.render('ps4Pug', { songLine: lyrics});
-                    console.log(lyrics);
-
                 }
             }
             );
